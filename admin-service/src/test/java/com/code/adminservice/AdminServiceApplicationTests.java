fbe3d41015dac22cae65e39fb35155eeeb96550d
nirali296 <108969880+nirali296@users.noreply.github.com>
2022-07-19 14:13:04 +0530

    Add files via upload

---
+++ admin-service/src/test/java/com/code/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ washer-service/src/main/java/com/code/washerservice/WasherServiceApplication.java
@@ -0,0 +1,13 @@
+package com.code.washerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasherServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasherServiceApplication.class, args);
+	}
+
+}
